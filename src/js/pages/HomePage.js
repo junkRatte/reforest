e9dc3d7a9bdc37918463e42fd614abd26e5e154e
junkRatte <59822502+junkRatte@users.noreply.github.com>
2021-12-09 17:25:36 +0100

    Created the projects section on the home page.

--- src/js/pages/HomePage.js
+++ src/js/pages/HomePage.js
@@ -1,13 +1,15 @@
 import React from "react";
 import HomePageAboutUs from "../sections/homepage/HomePageAboutUs";
 import HomePageHeader from "../sections/homepage/HomePageHeader";
+import HomePageProjects from "../sections/homepage/HomePageProjects";
 
 const Homepage = () => {
   return (
-    <>
+    <div className="homepage">
       <HomePageHeader />
       <HomePageAboutUs />
-    </>
+      <HomePageProjects />
+    </div>
   );
 };
 
---
+++ src/js/sections/homepage/HomePageProjects.js
@@ -0,0 +1,79 @@
+import React from "react";
+import ProjectsImage from "../../../images/homepage-projects-image.png";
+import ProjectsIconOne from "../../../images/projects-icon-one.png";
+import ProjectsIconTwo from "../../../images/projects-icon-two.png";
+import ProjectsIconThree from "../../../images/projects-icon-three.png";
+import ProjectsIconFour from "../../../images/projects-icon-four.png";
+
+const HomePageProjects = () => {
+  return (
+    <section className="homepage-projects">
+      <div className="container">
+        <div className="homepage-projects-summary">
+          <div className="summary-text">
+            <span className="heading-line"></span>
+            <div>
+              <h4>What we do</h4>
+              <h2>
+                We care for the earth, care for
+                <br />
+                the coming birth
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique.
+              </p>
+              <ul>
+                <li>
+                  <img src={ProjectsIconOne} />
+                  <div>
+                    <h3>Build Healthy Cities</h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse varius enim in eros.
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <img src={ProjectsIconTwo} />
+                  <div>
+                    <h3>Tree Plantation</h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse varius enim in eros.
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <img src={ProjectsIconThree} />
+                  <div>
+                    <h3>Protect Land and Water</h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse varius enim in eros.
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <img src={ProjectsIconFour} />
+                  <div>
+                    <h3>Animal Safety</h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse varius enim in eros.
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="summary-image">
+            <img src={ProjectsImage} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomePageProjects;
