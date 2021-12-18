ca10c62de427684e9064159d0d316e372fc8b3fb
junkRatte <59822502+junkRatte@users.noreply.github.com>
2021-12-18 11:08:21 +0100

    Created the awards section on the about page.

--- src/js/sections/aboutpage/AboutPageAwards.js
+++ src/js/sections/aboutpage/AboutPageAwards.js
@@ -1,11 +1,41 @@
 import React from "react";
 import AwardBadges from "../../components/AwardBadges";
+import TeamImage from "../../../images/team-image.png";
 
 const AboutPageAwards = () => {
   return (
     <div className="aboutpage-awards container">
       <h2>Awards & Recognitions</h2>
       <AwardBadges />
+      <div className="aboutpage-awards-team">
+        <div className="team-description">
+          <h5>Our journey</h5>
+          <h2>How we raised 34M</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique. Duis cursus, mi quis
+            viverra ornare, eros dolor interdum nulla, ut commodo diam libero
+            vitae erat. Aenean faucibus nibh.
+          </p>
+          <div className="team-description-stats">
+            <div>
+              <span>34M+</span>
+              <p>Donations Received</p>
+            </div>
+            <div>
+              <span>400+</span>
+              <p>Volunteers</p>
+            </div>
+            <div>
+              <span>40K+</span>
+              <p>Trees planted</p>
+            </div>
+          </div>
+        </div>
+        <div className="team-image">
+          <img src={TeamImage} />
+        </div>
+      </div>
     </div>
   );
 };
