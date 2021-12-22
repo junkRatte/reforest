05a69f6ce49ee030d30d75d24bc7abb6bf2fece2
junkRatte <59822502+junkRatte@users.noreply.github.com>
2021-12-22 15:49:38 +0100

    Created the news post page.

--- src/js/pages/NewsPostPage.js
+++ src/js/pages/NewsPostPage.js
@@ -6,16 +6,27 @@ const BlogPostPage = () => {
   let { id } = useParams();
 
   return (
-    <div>
+    <div className="news-post container">
       {NewsData.filter((news) => news.id == id).map((news) => (
         <>
-          <h1>{news.title}</h1>
+          <div className="news-post-title data-container">
+            <h5>{news.date}</h5>
+            <h1>{news.title}</h1>
+            <p>{news.introduction_text}</p>
+          </div>
           <img src={news.image} />
-          {/* <ul>
-            {news.pros.map((pros) => (
-              <li>{pros}</li>
-            ))}
-          </ul> */}
+          <div className="news-post-data data-container">
+            <h2>{news.description_title}</h2>
+            <p>{news.description}</p>
+            <h3>{news.pros_heading}</h3>
+            <ul>
+              {news.pros.map((pros) => (
+                <li>{pros}</li>
+              ))}
+            </ul>
+            <h3>{news.summary_title}</h3>
+            <p>{news.summary}</p>
+          </div>
         </>
       ))}
     </div>
