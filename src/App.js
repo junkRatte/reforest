4852ef3fa817900ad1f5f7b214da6594630ef7aa
junkRatte <59822502+junkRatte@users.noreply.github.com>
2022-01-13 13:06:53 +0100

    Removed routes component file.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import "./App.scss";
+import { Route, Routes } from "react-router-dom";
 import Footer from "./js/components/Footer";
 import Navbar from "./js/components/Navbar";
-import RoutesComponent from "./js/components/RoutesComponent";
+import Aboutpage from "./js/pages/AboutPage";
+import Homepage from "./js/pages/HomePage";
+import NewsPage from "./js/pages/NewsPage";
+import ProjectPostPage from "./js/pages/ProjectPostPage";
+import ProjectsPage from "./js/pages/ProjectsPage";
+import NewsPostPage from "./js/pages/NewsPostPage";
+import ContactPage from "./js/pages/ContactPage";
+import DonationPage from "./js/pages/DonationPage";
+import EventPostPage from "./js/pages/EventPostPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <RoutesComponent />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about-us" element={<Aboutpage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/projects/:title" element={<ProjectPostPage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/news/:title/:id" element={<NewsPostPage />} />
+        <Route path="/contact-us" element={<ContactPage />} />
+        <Route path="/donations" element={<DonationPage />} />
+        <Route path="/event/:title" element={<EventPostPage />} />
+      </Routes>
       <Footer />
     </div>
   );
